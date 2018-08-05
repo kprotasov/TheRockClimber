e3d62c927665b5782c8bb958c5b115b09e1478f8
kprotasov <kkprotasov@gmail.com>
2018-08-05 14:35:20 +0700

    добавлены файлы

---
+++ core/src/com/capcorn/games/therockclimber/OnShowRewardedVideoListener.java
@@ -0,0 +1,14 @@
+package com.capcorn.games.therockclimber;
+
+/**
+ * User: kprotasov
+ * Date: 05.12.2017
+ * Time: 22:27
+ */
+
+public interface OnShowRewardedVideoListener {
+
+    void onShowRewardedVideoForContinue();
+    void onShowRewardedVideoForX2();
+
+}
---
+++ core/src/com/capcorn/games/therockclimber/sprite/dialogs/PauseDialog.java
@@ -0,0 +1,18 @@
+package com.capcorn.games.therockclimber.sprite.dialogs;
+
+import com.capcorn.game.engine.sprite.Sprite;
+import com.capcorn.games.therockclimber.graphics.AssetsLoader;
+
+/**
+ * User: kprotasov
+ * Date: 02.05.2018
+ * Time: 14:51
+ */
+
+public class PauseDialog extends Sprite {
+
+    public PauseDialog(final AssetsLoader assetsLoader) {
+
+    }
+
+}
